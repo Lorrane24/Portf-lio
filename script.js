3d479ad6de8221f0fb123a4ba03d29f50868d392
Lorrane24 <lorranestory@gmail.com>
2025-07-03 13:08:05 -0300

    Primeiro upload
    Primeiro upolad do site

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const elementos = document.querySelectorAll('.animar-subir');
+
+  function animarSubir() {
+    const trigger = window.innerHeight * 0.85;
+
+    elementos.forEach(elem => {
+      const top = elem.getBoundingClientRect().top;
+      if (top < trigger) {
+        elem.classList.add('visible');
+      } else {
+        elem.classList.remove('visible');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', animarSubir);
+  animarSubir();
+});
